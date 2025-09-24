936af8a2731c2637c51dcd306e5a61881e19df9a
Carl Schéle <carl.schele@asurgent.se>
2025-09-24 10:01:05 +0200

    adding error page and dictionary item

---
+++ Controller/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using nackademin24_umbraco.Models.ViewModels;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace nackademin24_umbraco.Controller
+{
+    public class ErrorController : RenderController
+    {
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+        public ErrorController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _umbracoContextAccessor = umbracoContextAccessor;
+        }
+
+        public override IActionResult Index()
+        {
+            if (CurrentPage is Error errorPage)
+            {
+                var model = new ErrorPageViewModel(errorPage, _umbracoContextAccessor);
+
+                return View("error", model);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Models/ViewModels/ErrorPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace nackademin24_umbraco.Models.ViewModels
+{
+    public class ErrorPageViewModel : PageViewModel<Error>
+    {
+        public ErrorPageViewModel(Error content, IUmbracoContextAccessor umbracoContextAccessor) : base(content, umbracoContextAccessor)
+        {
+        }
+    }
+}
